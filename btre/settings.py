1f29cb8a034171cd016cad7cec345d491ebaee23
lindishabani <48189224+lindishabani@users.noreply.github.com>
2019-07-12 16:30:47 +0200

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -146,5 +146,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_POSRT = 587
 EMAIL_HOST_USER = 'lindshbn@gmail.com'
-EMAIL_HOST_PASSWORD = 'diella1.,'
+EMAIL_HOST_PASSWORD = '*********'
 EMAIL_USE_TLS = True
