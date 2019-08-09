792fba3af724cce5d83ec13e9f87e6c92f3fb58d
lindishabani <48189224+lindishabani@users.noreply.github.com>
2019-08-09 10:00:44 +0200

    Update views.py
    Adding login_required at dashboard view

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
 from contacts.models import Contact
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -66,7 +67,7 @@ def logout(request):
         messages.success(request, 'You are now logged out')
         return redirect('index')
 
-
+@login_required
 def dashboard(request):
     user_contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
 
